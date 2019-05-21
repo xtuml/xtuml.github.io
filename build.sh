48a10e0ab98cac1b63a849ecce4c669ad2f50b5d
Cortland Starrett <cort@roxsoftware.com>
2019-05-21 14:57:23 -0400

    job:  #11653  Themed html to be github-like.

--- build.sh
+++ build.sh
@@ -1,27 +1,28 @@
-asciidoctor ./bp_jumpstart/index.adoc -o ./bp_jumpstart/index.html
-#asciidoctor ./docs/xday.adoc -o ./docs/xday.html
-asciidoctor ./docs/xday2019.adoc -o ./docs/xday2019.html
-asciidoctor ./xtuml-class/class-prep/connect_shared_folders.adoc -o ./xtuml-class/class-prep/connect_shared_folders.html
-asciidoctor ./xtuml-class/class-prep/index.adoc -o ./xtuml-class/class-prep/index.html
-asciidoctor ./xtuml-class/crypto.adoc -o ./xtuml-class/crypto.html
-asciidoctor ./xtuml-class/sumo_edit/sumo_create5.adoc -o ./xtuml-class/sumo_edit/sumo_create5.html
-asciidoctor ./xtuml-class/sumo_edit/sumo_create4.adoc -o ./xtuml-class/sumo_edit/sumo_create4.html
-asciidoctor ./xtuml-class/sumo_edit/sumo_create3.adoc -o ./xtuml-class/sumo_edit/sumo_create3.html
-asciidoctor ./xtuml-class/sumo_edit/sumo_create2.adoc -o ./xtuml-class/sumo_edit/sumo_create2.html
-asciidoctor ./xtuml-class/sumo_edit/sumo_create1.adoc -o ./xtuml-class/sumo_edit/sumo_create1.html
-asciidoctor ./xtuml-class/sumo_edit/index.adoc -o ./xtuml-class/sumo_edit/index.html
-asciidoctor ./xtuml-class/sumo_start.adoc -o ./xtuml-class/sumo_start.html
-asciidoctor ./xtuml-class/sumo_project/index.adoc -o ./xtuml-class/sumo_project/index.html
-asciidoctor ./xtuml-class/homework/1.4.adoc -o ./xtuml-class/homework/1.4.html
-asciidoctor ./xtuml-class/homework/1.5.adoc -o ./xtuml-class/homework/1.5.html
-asciidoctor ./xtuml-class/homework/ev3_setup.adoc -o ./xtuml-class/homework/ev3_setup.html
-asciidoctor ./xtuml-class/homework/1.2.adoc -o ./xtuml-class/homework/1.2.html
-asciidoctor ./xtuml-class/homework/1.3.adoc -o ./xtuml-class/homework/1.3.html
-asciidoctor ./xtuml-class/homework/1.0.adoc -o ./xtuml-class/homework/1.0.html
-asciidoctor ./xtuml-class/homework/build_balancer.adoc -o ./xtuml-class/homework/build_balancer.html
-asciidoctor ./xtuml-class/homework/1.1.adoc -o ./xtuml-class/homework/1.1.html
-asciidoctor ./xtuml-class/homework/index.adoc -o ./xtuml-class/homework/index.html
-asciidoctor ./xtuml-class/index.adoc -o ./xtuml-class/index.html
-asciidoctor ./balancer/index.adoc -o ./balancer/index.html
-asciidoctor ./sumo/index.adoc -o ./sumo/index.html
-asciidoctor ./index.adoc -o ./index.html
+pwd=`pwd`
+asciidoctor -a stylesheet=$pwd/github-pandoc.css  ./bp_jumpstart/index.adoc -o ./bp_jumpstart/index.html
+#asciidoctor -a stylesheet=$pwd/github-pandoc.css  ./docs/xday.adoc -o ./docs/xday.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css  ./docs/xday2019.adoc -o ./docs/xday2019.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css  ./xtuml-class/class-prep/connect_shared_folders.adoc -o ./xtuml-class/class-prep/connect_shared_folders.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css  ./xtuml-class/class-prep/index.adoc -o ./xtuml-class/class-prep/index.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css  ./xtuml-class/crypto.adoc -o ./xtuml-class/crypto.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css  ./xtuml-class/sumo_edit/sumo_create5.adoc -o ./xtuml-class/sumo_edit/sumo_create5.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css  ./xtuml-class/sumo_edit/sumo_create4.adoc -o ./xtuml-class/sumo_edit/sumo_create4.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css  ./xtuml-class/sumo_edit/sumo_create3.adoc -o ./xtuml-class/sumo_edit/sumo_create3.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css  ./xtuml-class/sumo_edit/sumo_create2.adoc -o ./xtuml-class/sumo_edit/sumo_create2.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css  ./xtuml-class/sumo_edit/sumo_create1.adoc -o ./xtuml-class/sumo_edit/sumo_create1.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css  ./xtuml-class/sumo_edit/index.adoc -o ./xtuml-class/sumo_edit/index.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css  ./xtuml-class/sumo_start.adoc -o ./xtuml-class/sumo_start.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css  ./xtuml-class/sumo_project/index.adoc -o ./xtuml-class/sumo_project/index.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css  ./xtuml-class/homework/1.4.adoc -o ./xtuml-class/homework/1.4.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css  ./xtuml-class/homework/1.5.adoc -o ./xtuml-class/homework/1.5.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css  ./xtuml-class/homework/ev3_setup.adoc -o ./xtuml-class/homework/ev3_setup.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css  ./xtuml-class/homework/1.2.adoc -o ./xtuml-class/homework/1.2.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css  ./xtuml-class/homework/1.3.adoc -o ./xtuml-class/homework/1.3.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css  ./xtuml-class/homework/1.0.adoc -o ./xtuml-class/homework/1.0.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css  ./xtuml-class/homework/build_balancer.adoc -o ./xtuml-class/homework/build_balancer.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css  ./xtuml-class/homework/1.1.adoc -o ./xtuml-class/homework/1.1.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css  ./xtuml-class/homework/index.adoc -o ./xtuml-class/homework/index.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css  ./xtuml-class/index.adoc -o ./xtuml-class/index.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css  ./balancer/index.adoc -o ./balancer/index.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css  ./sumo/index.adoc -o ./sumo/index.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css  ./index.adoc -o ./index.html
