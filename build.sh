9c7ca5209abaf850ff69fbe10aede2aa61b986bb
Cortland Starrett <cort@roxsoftware.com>
2019-05-27 14:59:03 -0400

    job:  #11654  adding jp files for the class materials.

--- build.sh
+++ build.sh
@@ -3,16 +3,25 @@ asciidoctor -a stylesheet=$pwd/github-pandoc.css ./bp_jumpstart/bp_jumpstart.ado
 asciidoctor -a stylesheet=$pwd/github-pandoc.css ./xday/xday2018.adoc -o ./xday/xday2018.html
 asciidoctor -a stylesheet=$pwd/github-pandoc.css ./xday/xday2019.adoc -o ./xday/xday2019.html
 asciidoctor -a stylesheet=$pwd/github-pandoc.css ./xtuml_class/class_prep/connect_shared_folders.adoc -o ./xtuml_class/class_prep/connect_shared_folders.html
-asciidoctor -a stylesheet=$pwd/github-pandoc.css ./xtuml_class/class_prep/class_prep.adoc -o ./xtuml_class/class_prep/class_prep.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css -a jp=1 ./xtuml_class/class_prep/class_prep.adoc -o ./xtuml_class/class_prep/class_prep.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css ./xtuml_class/class_prep/class_prep.jp.adoc -o ./xtuml_class/class_prep/class_prep.jp.html
 asciidoctor -a stylesheet=$pwd/github-pandoc.css ./xtuml_class/crypto.adoc -o ./xtuml_class/crypto.html
-asciidoctor -a stylesheet=$pwd/github-pandoc.css ./xtuml_class/sumo_edit/sumo_create5.adoc -o ./xtuml_class/sumo_edit/sumo_create5.html
-asciidoctor -a stylesheet=$pwd/github-pandoc.css ./xtuml_class/sumo_edit/sumo_create4.adoc -o ./xtuml_class/sumo_edit/sumo_create4.html
-asciidoctor -a stylesheet=$pwd/github-pandoc.css ./xtuml_class/sumo_edit/sumo_create3.adoc -o ./xtuml_class/sumo_edit/sumo_create3.html
-asciidoctor -a stylesheet=$pwd/github-pandoc.css ./xtuml_class/sumo_edit/sumo_create2.adoc -o ./xtuml_class/sumo_edit/sumo_create2.html
-asciidoctor -a stylesheet=$pwd/github-pandoc.css ./xtuml_class/sumo_edit/sumo_create1.adoc -o ./xtuml_class/sumo_edit/sumo_create1.html
-asciidoctor -a stylesheet=$pwd/github-pandoc.css ./xtuml_class/sumo_edit/sumo_edit.adoc -o ./xtuml_class/sumo_edit/sumo_edit.html
-asciidoctor -a stylesheet=$pwd/github-pandoc.css ./xtuml_class/sumo_start.adoc -o ./xtuml_class/sumo_start.html
-asciidoctor -a stylesheet=$pwd/github-pandoc.css ./xtuml_class/sumo_project/sumo_project.adoc -o ./xtuml_class/sumo_project/sumo_project.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css -a jp=1 ./xtuml_class/sumo_edit/sumo_create5.adoc -o ./xtuml_class/sumo_edit/sumo_create5.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css ./xtuml_class/sumo_edit/sumo_create5.jp.adoc -o ./xtuml_class/sumo_edit/sumo_create5.jp.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css -a jp=1 ./xtuml_class/sumo_edit/sumo_create4.adoc -o ./xtuml_class/sumo_edit/sumo_create4.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css ./xtuml_class/sumo_edit/sumo_create4.jp.adoc -o ./xtuml_class/sumo_edit/sumo_create4.jp.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css -a jp=1 ./xtuml_class/sumo_edit/sumo_create3.adoc -o ./xtuml_class/sumo_edit/sumo_create3.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css ./xtuml_class/sumo_edit/sumo_create3.jp.adoc -o ./xtuml_class/sumo_edit/sumo_create3.jp.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css -a jp=1 ./xtuml_class/sumo_edit/sumo_create2.adoc -o ./xtuml_class/sumo_edit/sumo_create2.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css ./xtuml_class/sumo_edit/sumo_create2.jp.adoc -o ./xtuml_class/sumo_edit/sumo_create2.jp.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css -a jp=1 ./xtuml_class/sumo_edit/sumo_create1.adoc -o ./xtuml_class/sumo_edit/sumo_create1.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css ./xtuml_class/sumo_edit/sumo_create1.jp.adoc -o ./xtuml_class/sumo_edit/sumo_create1.jp.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css -a jp=1 ./xtuml_class/sumo_edit/sumo_edit.adoc -o ./xtuml_class/sumo_edit/sumo_edit.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css ./xtuml_class/sumo_edit/sumo_edit.jp.adoc -o ./xtuml_class/sumo_edit/sumo_edit.jp.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css -a jp=1 ./xtuml_class/sumo_start.adoc -o ./xtuml_class/sumo_start.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css ./xtuml_class/sumo_start.jp.adoc -o ./xtuml_class/sumo_start.jp.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css -a jp=1 ./xtuml_class/sumo_project/sumo_project.adoc -o ./xtuml_class/sumo_project/sumo_project.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css ./xtuml_class/sumo_project/sumo_project.jp.adoc -o ./xtuml_class/sumo_project/sumo_project.jp.html
 asciidoctor -a stylesheet=$pwd/github-pandoc.css -a jp=1 ./xtuml_class/homework/1.5.adoc -o ./xtuml_class/homework/1.5.html
 asciidoctor -a stylesheet=$pwd/github-pandoc.css ./xtuml_class/homework/1.5.jp.adoc -o ./xtuml_class/homework/1.5.jp.html
 asciidoctor -a stylesheet=$pwd/github-pandoc.css -a jp=1 ./xtuml_class/homework/1.4.adoc -o ./xtuml_class/homework/1.4.html
@@ -25,8 +34,10 @@ asciidoctor -a stylesheet=$pwd/github-pandoc.css -a jp=1 ./xtuml_class/homework/
 asciidoctor -a stylesheet=$pwd/github-pandoc.css ./xtuml_class/homework/1.1.jp.adoc -o ./xtuml_class/homework/1.1.jp.html
 asciidoctor -a stylesheet=$pwd/github-pandoc.css -a jp=1 ./xtuml_class/homework/1.0.adoc -o ./xtuml_class/homework/1.0.html
 asciidoctor -a stylesheet=$pwd/github-pandoc.css ./xtuml_class/homework/1.0.jp.adoc -o ./xtuml_class/homework/1.0.jp.html
-asciidoctor -a stylesheet=$pwd/github-pandoc.css ./xtuml_class/homework/ev3_setup.adoc -o ./xtuml_class/homework/ev3_setup.html
-asciidoctor -a stylesheet=$pwd/github-pandoc.css ./xtuml_class/homework/build_balancer.adoc -o ./xtuml_class/homework/build_balancer.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css -a jp=1 ./xtuml_class/homework/ev3_setup.adoc -o ./xtuml_class/homework/ev3_setup.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css ./xtuml_class/homework/ev3_setup.jp.adoc -o ./xtuml_class/homework/ev3_setup.jp.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css -a jp=1 ./xtuml_class/homework/build_balancer.adoc -o ./xtuml_class/homework/build_balancer.html
+asciidoctor -a stylesheet=$pwd/github-pandoc.css ./xtuml_class/homework/build_balancer.jp.adoc -o ./xtuml_class/homework/build_balancer.jp.html
 asciidoctor -a stylesheet=$pwd/github-pandoc.css -a jp=1 ./xtuml_class/homework/homework.adoc -o ./xtuml_class/homework/homework.html
 asciidoctor -a stylesheet=$pwd/github-pandoc.css ./xtuml_class/homework/homework.jp.adoc -o ./xtuml_class/homework/homework.jp.html
 asciidoctor -a stylesheet=$pwd/github-pandoc.css -a jp=1 ./xtuml_class/xtuml_class.adoc -o ./xtuml_class/xtuml_class.html
